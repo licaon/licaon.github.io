187481af1cb153907566c0995f34b425223597b5
Raul Macarie <raul.macarie@gmail.com>
2019-02-10 10:25:28 +0200

    Add review-app build

---
+++ review-app/precache-manifest.3e27f2dedea8db27ef3674d9c0547c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7692aabe9af1edf83372d548180b1c",
+    "url": "/review-app/static/media/image1.db7692aa.jpg"
+  },
+  {
+    "revision": "39ef5c7417db62ead3be",
+    "url": "/review-app/static/js/runtime~main.39ef5c74.js"
+  },
+  {
+    "revision": "901f19b9e1c518619dd9",
+    "url": "/review-app/static/js/main.901f19b9.chunk.js"
+  },
+  {
+    "revision": "4dbcd6828460f9ad8fa9",
+    "url": "/review-app/static/js/1.4dbcd682.chunk.js"
+  },
+  {
+    "revision": "901f19b9e1c518619dd9",
+    "url": "/review-app/static/css/main.4eb1b39a.chunk.css"
+  },
+  {
+    "revision": "71317571d400a0980b1477f24dfe0a00",
+    "url": "/review-app/index.html"
+  }
+];
